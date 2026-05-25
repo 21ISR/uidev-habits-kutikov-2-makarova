c83dd3c7dde45f4e5d3e78e2e77b97a7c789bbef
Макарова <ovppov29@gmail.com>
2026-05-25 06:25:17 +0000

    new

--- script.js
+++ script.js
@@ -2,11 +2,59 @@ const formEl = document.querySelector("form.form")
 const inputEl = document.querySelector(".form__input")
 const habitContainerEl = document.querySelector(".habit-list")
 
+function loadHabits(){
+    const data = localStorage.getItem('habits')
+
+    return data ? JSON.parse(data): []
+}
+function saveHabits(habits){
+    localStorage.setItem('habits', JSON.stringify(habits))
+}
+
+//let habits = []
+
+function renderHabit(habit){
+return`
+<li class="habit">
+    <div class="habit__header">
+        <h2 class="habit__name">${habit.name}</h2>
+                        <button class="habit__delete">&cross;</button>
+                    </div>
+                    <div class="habit__days"></div>
+                </li>
+                `
+}
+
+function render() {
+    const habits = loadHabits()
+    const habitsEl = habits.map(habit => renderHabit(habit)).join("")
+    habitContainerEl.innerHTML = habitsEl
+}
+
+
 function handleSubmit(e) {
     e.preventDefault()
-    habitContainerEl.innerHTML += inputEl.value
+    if (inputEl.value.trim() === '') return
+    const newHabit = {
+        name: inputEl.value.trim(),
+        id: Date.now()
+}
+
+    //habits.push(newHabit)
+    const oldHabits = loadHabits()
+
+    const newHabits = [...oldHabits, newHabit]
+
+    saveHabits(newHabits)
+
+    render()
 
     inputEl.value = ""
+
+    habitContainerEl.innerHTML += inputEl.value
+
 }
 
 formEl.addEventListener('submit', handleSubmit)
+
+render()
