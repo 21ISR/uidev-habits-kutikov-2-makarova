4201bdbf6c9eb2da6030a3ef1145cc3f01199a48
Макарова <ovppov29@gmail.com>
2026-05-27 03:09:11 +0000

    ne

--- script.js
+++ script.js
@@ -1,46 +1,63 @@
+const DAYS = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"]
+
 const formEl = document.querySelector("form.form")
 const inputEl = document.querySelector(".form__input")
 const habitContainerEl = document.querySelector(".habit-list")
 
-function loadHabits(){
+function loadHabits() {
     const data = localStorage.getItem('habits')
 
-    return data ? JSON.parse(data): []
+    return data ? JSON.parse(data) : []
 }
-function saveHabits(habits){
+
+function saveHabits(habits) {
     localStorage.setItem('habits', JSON.stringify(habits))
 }
 
-//let habits = []
+// let habits = []
 
-function renderHabit(habit){
-return`
-<li class="habit">
-    <div class="habit__header">
-        <h2 class="habit__name">${habit.name}</h2>
-                        <button class="habit__delete">&cross;</button>
-                    </div>
-                    <div class="habit__days"></div>
-                </li>
-                `
+function createDays(habit) {
+    return DAYS.map(
+        (day, i) => `
+            <div data-id=${habit.id} data-index=${i} class="day${habit.done.includes(i) ? " day--done" : ""}">
+                <span class="day__label">${day}</span>
+                <div class="day__circle"></div>
+            </div>
+        `,
+    ).join("")
 }
 
-function render() {
+function renderHabit(habit) {
+    return `
+    <li class="habit">
+        <div class="habit__header">
+            <h2 class="habit__name">${habit.name}</h2>
+            <button data-id=${habit.id} class="habit__delete">&cross;</button>
+        </div>
+        <div class="habit__days">${createDays(habit)}</div>
+    </li>
+    `
+}
+
+function render(){
     const habits = loadHabits()
+
     const habitsEl = habits.map(habit => renderHabit(habit)).join("")
     habitContainerEl.innerHTML = habitsEl
 }
 
-
 function handleSubmit(e) {
     e.preventDefault()
-    if (inputEl.value.trim() === '') return
-    const newHabit = {
-        name: inputEl.value.trim(),
-        id: Date.now()
-}
 
-    //habits.push(newHabit)
+    if(inputEl.value.trim() ==='') return
+
+    const newHabit = {
+        name:inputEl.value.trim(),
+        id: Date.now(),
+        done: [],
+    }
+
+    // habits.push(newHabit)
     const oldHabits = loadHabits()
 
     const newHabits = [...oldHabits, newHabit]
@@ -48,13 +65,47 @@ function handleSubmit(e) {
     saveHabits(newHabits)
 
     render()
-
+    
     inputEl.value = ""
-
-    habitContainerEl.innerHTML += inputEl.value
-
 }
 
+function handleHabit(e) {
+    const delHabitBtn = e.target.closest(".habit__delete")
+
+    if (delHabitBtn) {
+        const habitId = delHabitBtn.dataset.id
+
+        const oldHabits = loadHabits()
+
+        const newHabits = oldHabits.filter(el => el.id != habitId)
+
+        saveHabits(newHabits)
+        render()
+    }
+
+    const dayHabitBtn = e.target.closest(".day")
+
+    if (dayHabitBtn) {
+        const id = dayHabitBtn.dataset.id
+        const index = dayHabitBtn.dataset.index
+        const habits = loadHabits()
+
+        const newHabit = habits.map(el => {
+            if (el.id !== Number(id)) return el
+
+            const isDone = el.done.includes(index)
+
+            return {...el,
+                done: isDone ? el.done.filter(day => day !== index) : [...el.done, Number(index)]
+            }
+        })
+
+        saveHabits(newHabit)
+        render()
+    }
+}
+
+habitContainerEl.addEventListener("click", handleHabit)
 formEl.addEventListener('submit', handleSubmit)
 
 render()
